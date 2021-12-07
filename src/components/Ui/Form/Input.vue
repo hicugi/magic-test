0bad66b41d45f2924e2607b4a21a6d741d37acaa
Mag <hicugi@gmail.com>
2021-12-07 15:35:19 +0600

    fix: modelValue warning

--- src/components/Ui/Form/Input.vue
+++ src/components/Ui/Form/Input.vue
@@ -1,10 +1,22 @@
+<script setup>
+defineProps({
+  modelValue: String,
+});
+const emit = defineEmits(["update:modelValue"]);
+
+const handleInput = (e) => {
+  emit("update:modelValue", e.target.value);
+};
+</script>
+
 <template>
-  <input class="ui-formInput" v-model="modelValue" />
+  <input class="ui-formInput" v-model="modelValue" @input="handleInput" />
 </template>
 
 <style lang="sass">
 .ui-formInput
   border: 2px solid #E2E9F5
+  border-radius: 10px
   padding: 16px 26px 15px
   background: #EDF1F7
   color: #646876
