e7235b11dd5c37e51ddeb4b0e112546023bd17ab
Mag <hicugi@gmail.com>
2021-12-07 16:36:16 +0600

    feat: add absolute path

--- vite.config.js
+++ vite.config.js
@@ -1,3 +1,5 @@
+const path = require("path");
+
 import { defineConfig } from "vite";
 import vue from "@vitejs/plugin-vue";
 
@@ -8,4 +10,10 @@ export default defineConfig({
   },
 
   plugins: [vue()],
+
+  resolve: {
+    alias: {
+      "@": path.resolve(__dirname, "./src"),
+    },
+  },
 });
