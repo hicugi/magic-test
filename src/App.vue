79e6220904730f85050568f15943411fd7aa99f3
Mag <hicugi@gmail.com>
2024-08-11 13:25:38 +0500

    fix: remove unused background

--- src/App.vue
+++ src/App.vue
@@ -1,6 +1,5 @@
 <script setup>
 import AppBefore from "./components/App/Before.vue";
-import AppBackground from "./components/App/Background.vue";
 import Step from "./components/Step/index.vue";
 </script>
 
